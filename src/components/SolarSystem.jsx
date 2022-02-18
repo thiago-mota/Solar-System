b56ad80e458ea20e2411799263a546e260c2fa05
Thiago Mota <th.araujomota@gmail.com>
2022-02-18 16:45:48 -0300

    feat: renders a planet list on SolarSystem.jsx

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planets.map(({ name, image }) => (<PlanetCard
+          key={ name }
+          planetName={ name }
+          planetImage={ image }
+        />))}
       </div>
     );
   }
 }
 
 export default SolarSystem;
+
+// https://stackoverflow.com/questions/56758570/eslint-error-missing-parentheses-around-multilines-jsx-react-jsx-wrap-multiline
