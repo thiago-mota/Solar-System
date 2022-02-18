5295dc15af91d7fdfd341d7639ab34a6d1c0562d
Thiago Mota <th.araujomota@gmail.com>
2022-02-18 16:12:57 -0300

    feat: adds Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>
+        {headline}
+      </h2>
+    );
+  }
+}
+
+Title.propTypes = {
+  headline: propTypes.string.isRequired,
+};
+
+export default Title;
